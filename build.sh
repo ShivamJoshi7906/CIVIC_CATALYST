6cbbe996ee7ef45b4b696df20bf95ad2f3298d8b
ShivamJoshi7906 <shivamjoshi7906@gmail.com>
2026-03-19 00:19:53 +0530

    fix django-mongodb-backend version to 6.0.2

--- build.sh
+++ build.sh
@@ -3,9 +3,6 @@
 set -o errexit
 
 pip install --upgrade pip
-
-# Install with relaxed dependency checking to handle django-mongodb-backend conflict
-pip install -r requirements.txt --no-deps
-pip install -r requirements.txt --ignore-installed 2>/dev/null || true
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
